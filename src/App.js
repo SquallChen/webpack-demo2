bbf0301d33b55475ade23680e3e07f41a3e4feff
MR.C <442947365@qq.com>
2017-08-27 18:03:33 +0800

    完成注册登陆功能

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './reset.css'
 import TodoInput from './TodoInput'
 import TodoItem from './TodoItem'
 import UserDialog from './UserDialog'
+import {getCurrentUser, signOut} from './leanCloud'
 
 
 
@@ -14,6 +15,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
+      user:getCurrentUser() || {},
       newTodo: '',
       todoList:[]
     }
@@ -33,7 +35,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <h1>我的待办</h1>
+        <h1>{this.state.user.username||'我'}的待办
+          {this.state.user.id ? <buttom onClick={this.signOut.bind(this)}>登出</buttom>:null}
+        </h1>
         <div className="inputWrapper">
           <TodoInput content={this.state.newTodo}
           onChange={this.changeTitle.bind(this)} 
@@ -42,9 +46,25 @@ class App extends Component {
         <ol className="todoList">
           {todos}
         </ol>
-        <UserDialog />
+        {this.state.user.id ? 
+        null : 
+        <UserDialog 
+        onSignUp={this.onSignUpOrSignIn.bind(this)}
+        onSignIn={this.onSignUpOrSignIn.bind(this)}/>}
       </div>
     )
+  }
+  signOut(){
+    signOut()
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = {}
+    this.setState(stateCopy)
+  }
+ 
+  onSignUpOrSignIn(user){
+    let stateCopy = JSON.parse(JSON.stringify(this.state))
+    stateCopy.user = user
+    this.setState(stateCopy)
   }
   componentDidUpdate(){
      
--- src/UserDialog.js
+++ src/UserDialog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import './UserDialog.css'
-import {signUp} from './leanCloud'
+import {signUp,signIn} from './leanCloud'
 export default class UserDialog extends Component{
     constructor(props){
      super(props)
@@ -22,14 +22,44 @@ export default class UserDialog extends Component{
      e.preventDefault()
      let {username, password} = this.state.formData
      let success = (user)=>{
-       console.log(user)
+       this.props.onSignUp.call(null,user)
      }
      let error = (error)=>{
-       console.log(error)
+       switch(error.code){
+         case 202:
+         alert('用户名已被占用')
+         break
+          case 200:
+         alert('用户名不能为空')
+         break
+         default:
+         alert(error)
+         break
+       }
      }
      signUp(username, password, success, error)
    }
-    signIn(e){}
+    signIn(e){
+     e.preventDefault()
+     let {username, password} = this.state.formData
+     let success = (user)=>{
+       this.props.onSignUp.call(null,user) 
+    }
+    let error = (error)=>{
+      switch(error.code){
+        case 210:
+        alert('用户名与密码不匹配')
+        break
+        case 211:
+        alert('找不到用户')
+        break
+        default:
+        alert(error)
+        break
+      }
+    }
+    signIn(username,password,success,error)
+    }
     changeFormData(key,e){
       let stateCopy = JSON.parse(JSON.stringify(this.state))
       stateCopy.formData[key] = e.target.value
@@ -75,9 +105,15 @@ export default class UserDialog extends Component{
     return (
       <div className="UserDialog-Wrapper">
         <div className="UserDialog">
-          <nav onChange={this.switch.bind(this)}>
-              <label><input type="radio" value="signUp" checked={this.state.selected === 'signUp'}/> 注册</label>
-              <label><input type="radio" value="signIn" checked={this.state.selected === 'signIn'}/> 登录</label>
+          <nav>
+              <label><input type="radio" value="signUp" 
+              checked={this.state.selected === 'signUp'}
+              onChange={this.switch.bind(this)}
+              /> 注册</label>
+              <label><input type="radio" value="signIn" 
+              checked={this.state.selected === 'signIn'}
+              onChange={this.switch.bind(this)}
+              /> 登录</label>
           </nav>
           <div className="panes">
            {this.state.selected === 'signUp' ? signUpForm : null}
